df71ce98070c0ced6e7d8b9e6b3ce3220ce923ff
VinicCosta <viniciuscodp@gmail.com>
2022-12-28 00:09:36 -0300

    cria tela cadastro.

--- View/Principal.cs
+++ View/Principal.cs
@@ -1,3 +1,5 @@
+using Estoque.View;
+
 namespace Estoque
 {
     public partial class Principal : Form
@@ -5,6 +7,27 @@ namespace Estoque
         public Principal()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cadastro cadastro = new Cadastro();
+            cadastro.ShowDialog();
         }
     }
 }
