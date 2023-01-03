98cb5f3b3b17b449ac222eaf3b7f20af1d89bea7
Emmanuel Martins <emmanuelkta6@gmail.com>
2023-01-03 20:21:53 -0300

    Padroniza a StringConnection

---
+++ Services/ServiceConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque.Services
+{
+    public class ServiceConnection
+    {
+        
+        SqlConnection cn = new SqlConnection("Data Source=localhost;Initial Catalog=ESTOQUE;Integrated Security=True");
+        
+
+        public ServiceConnection(SqlConnection cn)
+        {
+            this.cn = cn;            
+        }
+
+        public static SqlConnection GetConnection()
+        {
+            return this.cn;
+        }
+
+        public string GetStringConnection()
+        {
+            return this.cn.ToString();
+        }
+    }
+
+}
